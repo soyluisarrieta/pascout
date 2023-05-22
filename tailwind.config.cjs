2426d6e028d43d405d1cbde62ddc24ae44bd0b87
Luis Arrieta <luisarrieta796@gmail.com>
2023-05-21 23:47:52 -0500

    Create a simple header component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      screens: {
+        xl: '1024px'
+      },
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        xl: '0rem'
+      }
+    },
+
     extend: {}
   },
   plugins: []
